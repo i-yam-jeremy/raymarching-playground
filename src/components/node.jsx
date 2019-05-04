6124a6158dfdf226f974b1e9afbc5a365b802334
i-yam-jeremy <i-yam-jeremy@users.noreply.github.com>
2019-05-04 16:27:59 -0700

    Labeled inputs to node

--- src/components/node.jsx
+++ src/components/node.jsx
@@ -3,8 +3,12 @@ import React from 'react'
 class Input extends React.Component {
   render() {
     return (
-      <div style={{width: '16px', height: '16px', 'border-radius': '16px', 'border': '2px solid red', 'position': 'absolute', 'left': '0px', 'top': (20*this.props.index) + 'px'}}>
-        {this.props.index}
+      <div>
+        <div style={{backgroundColor: '#555555', color: '#FFFFFF', width: '16px', height: '16px', borderRadius: '16px', border: '2px solid #FFA500', position: 'absolute', left: '-10px', top: (25/2 + 25*this.props.index) + 'px', margin: '0px', padding: '0px'}}>
+        </div>
+        <div style={{position: 'absolute', left: '16px', 'top': (25/2 + 25*this.props.index) + 'px', margin: '0px', padding: '0px'}}>
+          {this.props.inputName}
+        </div>
       </div>
     )
   }
@@ -14,10 +18,9 @@ export default class Node extends React.Component {
 
   render() {
     return (
-      <div style={{textAlign: 'center'}}>
-        <h1>Hello World</h1>
+      <div style={{backgroundColor: '#555555', color: '#FFFFFF', width: '100px', height: (this.props.inputs.length*25 + 25) + 'px', borderRadius: '16px', border: '2px solid black', position: 'absolute', margin: '0px', paddingLeft: '70px'}}>
         <div>
-          {this.props.inputs.map((input, i) => <Input key={i} index={i} />)}
+          {this.props.inputs.map((inputName, i) => <Input key={'input-' + inputName} index={i} inputName={inputName} />)}
         </div>
       </div>
     )
--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Node from './components/node.jsx';
 
-ReactDOM.render(<Node inputs={[1, 2, 3, 4]} />, document.body);
+ReactDOM.render(<Node inputs={['radius', 'center']} />, document.body);
