dd007e1134043ca608b83a71b55ee121ee70a9df
i-yam-jeremy <i-yam-jeremy@users.noreply.github.com>
2019-05-20 17:52:41 -0700

    Added rotation transform node

--- src/components/node/node-types/vector/index.js
+++ src/components/node/node-types/vector/index.js
@@ -1,7 +1,8 @@
 import Translation from './translation.jsx'
+import Rotation from './rotation.jsx'
 import Vec3 from './vec3.jsx'
 import X from './x.jsx'
 import Y from './y.jsx'
 import Z from './z.jsx'
 
-export default [Translation, Vec3, X, Y, Z]
+export default [Translation, Rotation, Vec3, X, Y, Z]
