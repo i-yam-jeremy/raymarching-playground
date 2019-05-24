dd22f1429a704ff847ad0bf033e731dbe7a8d494
i-yam-jeremy <i-yam-jeremy@users.noreply.github.com>
2019-05-24 16:26:59 -0700

    Restyled render HUD time component

--- src/components/render/render-hud/time.jsx
+++ src/components/render/render-hud/time.jsx
@@ -7,10 +7,43 @@ function roundNumber(n, decimalPlaces) {
 
 export default class Time extends React.Component {
 
+  getRestartButton() {
+    return (
+      <svg width={24} height={32} xmlns="http://www.w3.org/2000/svg">
+        <polygon points="24,0 0,16 24,32" fill="#BBBBBB" />
+      </svg>
+    )
+  }
+
+  getPlayButton() {
+    return (
+      <svg width={24} height={32} xmlns="http://www.w3.org/2000/svg">
+        <polygon points="0,0 24,16 0,32" fill="#BBBBBB" />
+      </svg>
+    )
+  }
+
+  getPauseButton() {
+    return (
+      <svg width={24} height={32} xmlns="http://www.w3.org/2000/svg">
+        <polygon points="2,0 8,0 8,32 2,32" fill="#BBBBBB" />
+        <polygon points="16,0 22,0 22,32 16,32" fill="#BBBBBB" />
+      </svg>
+    )
+  }
+
   render() {
     return (
-      <div>
-        <p>Time: <span onClick={this.props.restartTime}>◀</span><span onClick={this.props.toggleTimePlaying} style={{fontWeight: '900'}}>{this.props.timePlaying ? '||' : '▶'}</span>{roundNumber(this.props.time, 1)}</p>
+      <div className="render-hud-time-container">
+        <div className="render-hud-time-control" onClick={this.props.restartTime}>
+          {this.getRestartButton()}
+        </div>
+        <div className="render-hud-time-control" onClick={this.props.toggleTimePlaying}>
+          {this.props.timePlaying ? this.getPauseButton() : this.getPlayButton()}
+        </div>
+        <div className="render-hud-time-number">
+          {roundNumber(this.props.time, 1)}
+        </div>
       </div>
     )
   }
