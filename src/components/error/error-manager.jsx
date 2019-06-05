ff80789143d8720f302081a8794a1b77d93057fb
i-yam-jeremy <i-yam-jeremy@users.noreply.github.com>
2019-06-05 11:22:29 -0700

    Styled error messages

--- src/components/error/error-manager.jsx
+++ src/components/error/error-manager.jsx
@@ -1,10 +1,26 @@
+import React from 'react'
 import toast from 'toasted-notes'
 import 'toasted-notes/src/styles.css'
+
+class ErrorMessage extends React.Component {
+
+  render() {
+    return (
+      <div className="error-message-container">
+        <div className="message">{this.props.message}</div>
+        <div className="close" onClick={this.props.onClose}>×</div>
+      </div>
+    )
+  }
+
+}
 
 class ErrorManager {
 
   static error(message) {
-    toast.notify(message)
+    toast.notify(({ onClose }) => (
+      <ErrorMessage message={message} onClose={onClose} />
+    ))
     throw message
   }
 
