b3ec8ae10d5e55a34e6c1f349bce4e6d84ff2440
i-yam-jeremy <i-yam-jeremy@users.noreply.github.com>
2019-06-06 10:28:57 -0700

    Made region overlapsWith slightly better

--- src/components/node/node-editor-panel.jsx
+++ src/components/node/node-editor-panel.jsx
@@ -264,7 +264,11 @@ class Region {
       this.contains(other.x, other.y) ||
       this.contains(other.x + other.width, other.y) ||
       this.contains(other.x + other.width, other.y + other.height) ||
-      this.contains(other.x, other.y + other.height)
+      this.contains(other.x, other.y + other.height) ||
+      other.contains(this.x, this.y) ||
+      other.contains(this.x + this.width, this.y) ||
+      other.contains(this.x + this.width, this.y + this.height) ||
+      other.contains(this.x, this.y + this.height)
     )
   }
 
